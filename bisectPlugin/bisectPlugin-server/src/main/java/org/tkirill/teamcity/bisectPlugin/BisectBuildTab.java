664162f0357bb39fd1f76d7c625ffc59bb76dcda
Kirill Teplinskiy <tkirill128@gmail.com>
2015-06-04 15:43:55 +0500

    Add empty bisect tab.

---
+++ bisectPlugin/bisectPlugin-server/src/main/java/org/tkirill/teamcity/bisectPlugin/BisectBuildTab.java
@@ -0,0 +1,21 @@
+package org.tkirill.teamcity.bisectPlugin;
+
+import jetbrains.buildServer.serverSide.BuildsManager;
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.web.openapi.BuildTab;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.WebControllerManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class BisectBuildTab extends BuildTab {
+    protected BisectBuildTab(WebControllerManager manager, BuildsManager buildManager, PluginDescriptor myDescriptor) {
+        super("bisectBuildTab", "Bisect", manager, buildManager, myDescriptor.getPluginResourcesPath("BisectBuildTab.jsp"));
+    }
+
+    @Override
+    protected void fillModel(@NotNull Map<String, Object> model, @NotNull SBuild build) {
+
+    }
+}
